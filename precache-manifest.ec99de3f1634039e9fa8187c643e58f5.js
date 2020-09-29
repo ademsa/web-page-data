42cc522a5ed4a15c5c6f373c2c6d6335dac62bdc
Adem <63111861+ademsa@users.noreply.github.com>
2020-09-29 19:16:35 +0200

    v0.1.1

---
+++ precache-manifest.ec99de3f1634039e9fa8187c643e58f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e72a00fee3d931efaff599dedb753a8a",
+    "url": "/web-page-data/index.html"
+  },
+  {
+    "revision": "68a97adefaa238599ad0",
+    "url": "/web-page-data/static/css/main.79f7805e.chunk.css"
+  },
+  {
+    "revision": "6664ad299287577725f6",
+    "url": "/web-page-data/static/js/2.c1fd7340.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/web-page-data/static/js/2.c1fd7340.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68a97adefaa238599ad0",
+    "url": "/web-page-data/static/js/main.f771e75a.chunk.js"
+  },
+  {
+    "revision": "760604a785cce53edb14",
+    "url": "/web-page-data/static/js/runtime-main.b1cba452.js"
+  }
+]);
