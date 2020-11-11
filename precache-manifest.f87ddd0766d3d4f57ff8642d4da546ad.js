3ad24614131ab6644531d1cf783a91665d31a19d
Adem <63111861+ademsa@users.noreply.github.com>
2020-11-11 13:29:18 +0100

    v0.1.4

--- precache-manifest.f87ddd0766d3d4f57ff8642d4da546ad.js
+++ precache-manifest.f87ddd0766d3d4f57ff8642d4da546ad.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "cef51945cb52fd8de047809a9db415c6",
+    "revision": "64053638b8b98cddecb7d68078731ff9",
     "url": "/web-page-data/index.html"
   },
   {
-    "revision": "ca0b0f2b4d751ecad7ba",
-    "url": "/web-page-data/static/css/main.3fa3b5c2.chunk.css"
+    "revision": "6995757d869b4583dad1",
+    "url": "/web-page-data/static/css/main.3cef065e.chunk.css"
   },
   {
     "revision": "6664ad299287577725f6",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/web-page-data/static/js/2.c1fd7340.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "ca0b0f2b4d751ecad7ba",
-    "url": "/web-page-data/static/js/main.10e575f2.chunk.js"
+    "revision": "6995757d869b4583dad1",
+    "url": "/web-page-data/static/js/main.efdf5337.chunk.js"
   },
   {
     "revision": "760604a785cce53edb14",
