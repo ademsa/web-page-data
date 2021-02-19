c09ec1151fbaad6fe46a070cdb7e97ae3733cd15
Adem <63111861+ademsa@users.noreply.github.com>
2021-02-19 11:20:48 +0100

    Improvements 2 (February 2021)
    * Updated Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,9 @@ export default function Footer() {
             <footer className="footer py-5">
                 <div className="content has-text-centered grey-light">
                     <p className="has-text-weight-light my-0 is-size-7">
-                        Source code available <a href="http://github.com/ademsa/web-page-data">here</a> (<a href="https://github.com/ademsa/web-page-data/blob/master/LICENSE.md">MIT license</a>).
+                        Source code available <a href="https://github.com/ademsa/web-page-data">here</a> (<a href="https://github.com/ademsa/web-page-data/blob/master/LICENSE.md">MIT license</a>).
+                        <br />
+                        Hosted on <a href="https://pages.github.com/">GitHub Pages</a>
                     </p>
                     <br />
                     <a className="mt-2" href="https://ademsabic.com">
